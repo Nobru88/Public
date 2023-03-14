a8503563cd1d3a6eb84cc1d9e031851acbaaa85c
Buno Valerio <125913766+Nobru88@users.noreply.github.com>
2023-03-14 17:52:32 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,96 +1,96 @@
-const myModal = new bootstrap.Modal("#register-modal");
+const myModal = new bootstrap.Modal("#exampleModal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
 
 checkLogged();
 
 //LOGAR NO SISTEMA
-document.getElementById("login-form").addEventListener("submit", function(e) {
-    e.preventDefault(); 
+document.getElementById("login-form").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const email = document.getElementById("email-input").value;
-    const password = document.getElementById("password-input").value;
-    const checkSession = document.getElementById("session-check").checked;
+  const email = document.getElementById("email-input").value;
+  const password = document.getElementById("password-input").value;
+  const checksession = document.getElementById("session-check").checked;
 
-    const account = getAccount(email);
+  const account = getAccount(email)
 
-    if(!account) {
-        alert("Opps! Verifique o usuário ou a senha.");
-        return;
+  if (!account) {
+    alert("Opps! Verifique o usuário ou a senha.");
+    return;
+  }
+
+  if (account) {
+    if (account.password !== password) {
+      alert("Opps! Verifique o usuário ou a senha.");
+      return;
     }
 
-    if(account) {
-        if(account.password !== password) {
-            alert("Opps! Verifique o usuário ou a senha.");
-        return;
-        }
+    saveSession(email, checksession);
 
-        saveSession(email, checkSession);
+    window.location.href = "home.html";
 
-        window.location.href = "home.html";
-    }
+  }
 });
 
 //CRIAR CONTA
-document.getElementById("create-form").addEventListener("submit", function(e) {
-    e.preventDefault(); 
+document.getElementById("create-form-login").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const email = document.getElementById("email-create-input").value;
-    const password = document.getElementById("password-create-input").value;
+  const email = document.getElementById("email-create-input").value;
+  const password = document.getElementById("password-create-input").value;
 
-    if(email.length < 5) {
-        alert("Preencha o campo com um e-mail válido");
-        return;
-    }
-    
-    if(password.length < 4) {
-        alert("Preencha a senha com no minimo 4 digitos")
-        return;
-    }
+  if (email.length < 5) {
+    alert("Preencha o campo com um email válido.");
+    return;
+  }
 
-    saveAccount({
-        login: email,
-        password: password,
-        transactions: []
+  if (password.length < 4) {
+    alert("preencha a senha com no mínimo 6 dígitos.");
+    return;
+  }
 
-    });
-    
-    myModal.hide();
+  salvarConta({
+    login: email,
+    password: password,
+    transactions: [],
+  });
 
-    alert("Conta criada com sucesso!");
-    
+  myModal.hide();
 
- });
+  alert("conta criada com sucesso.");
+});
+
 function checkLogged() {
-    if(session) {
-        sessionStorage.setItem("logged", session);
-        logged = session;
-    }
+  if (session) {
+    sessionStorage.setItem("logged", session);
+    logged = session;
+  }
 
-    if(logged) {
-        saveSession(logged, session);
+  if (logged) {
+    saveSession(logged, session);
 
-        window.location.href = "home.html";
-    }
+    window.location.href = "home.html";
+  }
 }
 
- function saveAccount(data) {
-     localStorage.setItem(data.login, JSON.stringify(data));
-    }
+function salvarConta(data) {
+  localStorage.setItem(data.login, JSON.stringify(data));
+}
 
-    function saveSession(data, saveSession) {
-        if(saveSession) {
-            localStorage.setItem("session", data);
-        }
+function saveSession(data, saveSession) {
+  if (saveSession) {
+    localStorage.setItem("session", data);
+  }
 
-        sessionStorage.setItem("logged", data);
-    }
+  sessionStorage.setItem("logged", data);
+}
 
-function getAccount(Key) {
-    const account = localStorage.getItem(Key);
-    if(account) {
-        return JSON.parse(account);
-    }
+function getAccount(key) {
+  const account = localStorage.getItem(key);
 
-    return "";
-}     
+  if (account) {
+    return JSON.parse(account);
+  }
+
+  return "";
+}
